e72f2630c0faa654ee5178711598c4b512e09cac
The Verse <floatbeta@gmail.com>
2024-07-30 23:19:48 -0500

    Create tailwind.config.js

---
+++ js/tailwind.config.js
@@ -0,0 +1,13 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  // ...
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
+      addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &'])
+      addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
+      addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &'])
+    }),
+  ],
+}
